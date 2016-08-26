9babd82af20db2a9e933f88610c3c4c8f256bca7
yuxiaohua <hzyuxiaohua@corp.netease.com>
2016-08-26 22:31:16 +0800

    add RxSwift notation combineLatest\zip\startWith

--- RxDemo/CaculatorViewController.swift
+++ RxDemo/CaculatorViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class CaculatorViewController: UIViewController {
 
@@ -15,12 +17,38 @@ class CaculatorViewController: UIViewController {
     @IBOutlet weak var num3: UITextField!
     @IBOutlet weak var sum: UITextField!
     
+    var disposeBag: DisposeBag?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Caculator"
+        
+        self.disposeBag = DisposeBag()
+        
         // Do any additional setup after loading the view.
+//        Observable.combineLatest(num1.rx_text, num2.rx_text, num3.rx_text) { (text1, text2, text3) -> Int in
+//            return (Int(text1) ?? 0) + (Int(text2) ?? 0) + (Int(text3) ?? 0)
+//        }
+//        .map { $0.description }
+//        .bindTo(sum.rx_text)
+//        .addDisposableTo(self.disposeBag!)
+        
+//        Observable.zip(num1.rx_text, num2.rx_text, num3.rx_text) { (text1, text2, text3) -> Int in
+//            return (Int(text1) ?? 0) + (Int(text2) ?? 0) + (Int(text3) ?? 0)
+//        }
+//        .map { (num) -> String in
+//            return num.description
+//        }
+//        .bindTo(sum.rx_text)
+//        .addDisposableTo(self.disposeBag!)
+        
+        let source = Observable.range(start: 0, count: 3)
+        let result = source.startWith(-3, -2, -1)
+        result.subscribe { (event) in
+            if let num = event.element {
+                print(num)
+            }
+        }
     }
-    
-    
 
 }

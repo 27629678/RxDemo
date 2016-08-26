23f625070687631305778872aaf216bb525a5f2d
yuxiaohua <hzyuxiaohua@corp.netease.com>
2016-08-26 19:41:43 +0800

    add caculator vc

---
+++ RxDemo/CaculatorViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CaculatorViewController.swift
+//  RxDemo
+//
+//  Created by hzyuxiaohua on 8/26/16.
+//  Copyright © 2016 hzyuxiaohua. All rights reserved.
+//
+
+import UIKit
+
+class CaculatorViewController: UIViewController {
+
+    @IBOutlet weak var num1: UITextField!
+    @IBOutlet weak var num2: UITextField!
+    @IBOutlet weak var num3: UITextField!
+    @IBOutlet weak var sum: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Caculator"
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
--- RxDemo/ViewController.swift
+++ RxDemo/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var titles = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "RxDemos"
+        
+        titles.append("Caculator")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: table view delegate and datasource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("RxDemos")
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: "RxDemos")
+        }
+        
+        cell?.textLabel?.text = titles[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        if titles[indexPath.row] == "Caculator" {
+            let target = storyboard?.instantiateViewControllerWithIdentifier("CaculatorViewController")
+            self.navigationController?.pushViewController(target!, animated: true)
+        }
+    }
 
 }
 
